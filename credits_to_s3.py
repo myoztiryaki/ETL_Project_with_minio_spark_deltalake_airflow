1bcf43b4666eaee2bd5906df4441a42ab7226855
Merve Öztiryaki <126799485+myoztiryaki@users.noreply.github.com>
2024-07-22 17:42:00 +0300

    Update credits_to_s3.py

--- credits_to_s3.py
+++ credits_to_s3.py
@@ -63,7 +63,6 @@ df_credits1 = df_credits1.withColumn("crew", from_json(col("crew"), json_crew))
 
 df_credits1.printSchema()
 
-#explode_outer ile iç içe geçmiş arrayi düzleştirdim veistenen değerleri credits_cast tablosunda gösterdim/ explode_outerda boş veya null durumundaki satırlar korunur
 credits_cast = df_credits1.select("movie_id", "title", explode_outer("cast").alias("cast"))
 credits_cast = credits_cast.select("movie_id", "title", "cast.cast_id", "cast.character", "cast.credit_id", "cast.gender", "cast.id", "cast.name")
 credits_cast = credits_cast.withColumn("movie_id", col("movie_id").cast("string"))
@@ -74,8 +73,6 @@ credits_cast = credits_cast.fillna({'credit_id': 0000000000})
 # tekrarlanan satırları sil
 un_credits_cast = credits_cast.dropDuplicates(['movie_id', 'title','cast_id', 'character', 'credit_id','id', 'name'])
 
-
-#explode_outer ile iç içe geçmiş arrayi düzleştirdim veistenen değerleri credits_crew tablosunda gösterdim/ explode_outerda boş veya null durumundaki satırlar korunur
 credits_crew = df_credits1.select("movie_id", "title", explode_outer("crew").alias("crew"))
 credits_crew = credits_crew.select("movie_id", "title", "crew.credit_id", "crew.department", "crew.gender", "crew.id", "crew.job", "crew.name")
 credits_crew = credits_crew.withColumn("movie_id", col("movie_id").cast("string"))
@@ -86,22 +83,18 @@ credits_crew = credits_crew.fillna({'credit_id': 0000000000})
 # tekrarlanan satırları sil
 un_credits_crew = credits_crew.dropDuplicates(['movie_id', 'id','credit_id'])
 
-
-
 cast_deltaPath = "s3a://tmdb-silver/cast"
 cast_delta = DeltaTable.forPath(spark, cast_deltaPath)
 
 crew_deltaPath = 's3a://tmdb-silver/crew'
 crew_delta = DeltaTable.forPath(spark, crew_deltaPath)
 
-#minio upsert
 cast_delta.alias("cast") \
     .merge(un_credits_cast.alias("cast_new"), "cast.movie_id = cast_new.movie_id AND cast.credit_id = cast_new.credit_id") \
     .whenMatchedUpdateAll() \
     .whenNotMatchedInsertAll() \
     .execute()
 
-#minio upsert
 crew_delta.alias("crew") \
     .merge(un_credits_crew.alias("crew_new"), "crew.movie_id = crew_new.movie_id AND crew.credit_id = crew_new.credit_id") \
     .whenMatchedUpdateAll() \
